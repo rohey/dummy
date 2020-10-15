4f758aa031b7e965616262651d2f907379bd996a
sparky <yehor.cpp@yahoo.com>
2020-10-15 16:54:46 +0800

    update

--- api.py
+++ api.py
@@ -9,11 +9,36 @@ import numpy as np
 from torchvision import transforms
 from PIL import Image
 
+
+import gluoncv
+import mxnet as mx
+import os
+from gluoncv.data.transforms.presets.segmentation import test_transform
+
+ctx = mx.cpu()
+
+def segmentation_mask(model, img):
+    # using cpu
+    img = mx.nd.array(np.asarray(img))
+    img = test_transform(img, ctx)
+    output = model.predict(img)
+    predict = mx.nd.squeeze(mx.nd.argmax(output, 1)).asnumpy() == 15.
+
+    # from gluoncv.utils.viz import get_color_pallete
+    # mask = get_color_pallete(predict, 'pascal_voc')
+
+    # mask.save('output.png')
+    # mmask = mpimg.imread('output.png')
+    # plt.imshow(mask)
+    # plt.show()
+
+    return predict
+
 class Model(nn.Module):
 
     '''
-    TRAIN LOSS:  15.156542003154755
-    VALID LOSS:  50.013286113739014 49.15118074417114
+    TRAIN LOSS:  3.041159379899558
+    VALID LOSS:  2.540838477733394 2.5285385551957966
     '''
 
     def __init__(self):
@@ -21,7 +46,7 @@ class Model(nn.Module):
         self.extractor = torchvision.models.resnet18(True)
         self.embd = nn.Embedding(3, 5)
 
-        self.regressor = nn.Linear(520, 19)
+        self.regressor = nn.Sequential(nn.Linear(520, 1024), nn.Dropout(0.5), nn.Linear(1024, 19))
 
     def forward(self, img, gender, height, weight, age):
         with torch.no_grad():
@@ -30,10 +55,9 @@ class Model(nn.Module):
             x = self.extractor.relu(x)
             x = self.extractor.maxpool(x)
 
-            x = self.extractor.layer1(x)
-            x = self.extractor.layer2(x)
-            x = self.extractor.layer3(x)
-
+        x = self.extractor.layer1(x)
+        x = self.extractor.layer2(x)
+        x = self.extractor.layer3(x)
         x = self.extractor.layer4(x)
         x = self.extractor.avgpool(x)
 
@@ -79,6 +103,8 @@ class Predictor:
         self.model = Model().eval().cuda()
         self.model.load_state_dict(state_dict)
 
+        self.bkgmodel = gluoncv.model_zoo.get_model('fcn_resnet101_voc', pretrained=True, ctx=ctx)
+
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
 
@@ -92,10 +118,18 @@ class Predictor:
 
 
     def run(self, gender_value, height_value, weight_value, age_value, img):
-        img = self.transform(img).unsqueeze(0)
+        mask = segmentation_mask(self.bkgmodel, img)
+        img = np.array(img)
+        img[~mask] = 0
+        img = Image.fromarray(img)
+        img = self.transform(img)
+
+
 
         # plt.imshow(img.transpose(0, 2).transpose(0, 1).cpu().detach().numpy())
         # plt.show()
+
+        img = img.unsqueeze(0)
 
 
         out = self.model.forward(img.cuda(), gender_value.cuda(), height_value.cuda(),
@@ -108,10 +142,11 @@ class Predictor:
 if __name__ == '__main__':
 
     gender = torch.tensor([0]) # 0 - female; 1 - male; 2 - who knows
-    height = torch.tensor([[158]]) # cm
-    weight = torch.tensor([[50]]) # kg
-    age = torch.tensor([[27]]) # seconds.. joke. years.
-    img = Image.open('1249-19_7.jpg') #  photo
+    height = torch.tensor([[160]]) # cm
+    weight = torch.tensor([[90]]) # kg
+    age = torch.tensor([[38]]) # seconds.. joke. years.
+    img = Image.open('249480261.jpg') #  photo
+    img = transforms.Resize(256)(img)
 
     state_dict  = torch.load("state_dict.torch")
 
